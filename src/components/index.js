8dbd9abd2008d2b82dc5110ed15858f5d649693e
Piyushsharma2002 <piyush@rendernet.ai>
2026-01-07 19:48:34 +0530

    chore(cleanup): remove TypeScript artifacts and add components index; polish styles

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as PaymentSuccess } from './PaymentSuccess';
+export { default as RewardsContainer } from './RewardsContainer';
+export { default as RewardProgress } from './RewardProgress';
+export { default as RewardScratchCard } from './RewardScratchCard';
+export { default as SuccessHeader } from './SuccessHeader';
--- src/custom.d.ts
+++
@@ -1,2 +0,0 @@
-// Project converted to plain JavaScript. TypeScript declaration files removed.
-// If you add TypeScript later, add appropriate declarations here.
